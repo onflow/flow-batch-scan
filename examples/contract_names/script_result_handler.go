1c0ee3f9fe01070f2d1ef80bd60bdb2c62bddca2
Janez Podhostnik <janez.podhostnik@gmail.com>
2023-01-19 21:13:06 +0100

    Fixed licencing

--- examples/contract_names/script_result_handler.go
+++ examples/contract_names/script_result_handler.go
@@ -1,3 +1,17 @@
+// Copyright 2023 Dapper Labs, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package main
 
 import (
@@ -59,7 +73,7 @@ func (r *scriptResultHandler) Handle(out fbs.ProcessedAddressBatch) error {
 			Msg("Address contracts")
 	}
 
-	for address, _ := range okAddresses {
+	for address := range okAddresses {
 		if record, ok := r.duplicatedContracts[address]; ok && record.BlockHeight >= out.BlockHeight {
 			continue
 		}
