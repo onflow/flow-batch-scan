ce77ac9872798120e63136468bbde137d18e8cbf
Janez Podhostnik <67895329+janezpodhostnik@users.noreply.github.com>
2023-01-30 22:02:00 +0000

    Fix merge list (#12)

--- utils/sets.go
+++ utils/sets.go
@@ -14,11 +14,12 @@
 
 package utils
 
-func MergeInto[K comparable, V any](dest map[K]V, from map[K]V) {
+func MergeInto[K comparable, V any](dest map[K]V, from map[K]V) map[K]V {
 	if dest == nil {
 		dest = make(map[K]V)
 	}
 	for k, v := range from {
 		dest[k] = v
 	}
+	return dest
 }
